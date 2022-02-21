434f6a2fe16e645ae066ddcc705141df47d4a630
unknown <robelged1992@gmail.com>
2022-02-20 16:34:10 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# student_score={
+#     "harry": 81,
+#     "ron": 78,
+#     "hermi": 99,
+#     "draco": 30
+# }
+# student_grade={}
+#
+# for key in student_score:
+#     score = student_score[key]
+#     if score > 90:
+#         student_grade[key] = "outstanding"
+#     elif score > 80:
+#         student_grade[key] = "excellent"
+#     elif score > 70:
+#         student_grade[key] = "not bad"
+#     elif score <50:
+#         student_grade[key] = "u should just die"
+#
+#
+# print(student_grade)
+
+
+travel_log = {
+    "france": {"city_visited": ["paris", "lille", "dijon"] },
+    "germeny":"berlin"
+
+}
